4bccf8d1eec121284af208e1687feacafa57a301
DeepakParari <dpak666@gmail.com>
2020-07-15 16:27:00 +0200

    Added Delete post test

--- src/test/java/tests/Posts.java
+++ src/test/java/tests/Posts.java
@@ -1,12 +1,9 @@
 package tests;
 
-import helpers.JSONHelper;
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import pojos.Post;
-import pojos.User;
 import services.PostsService;
 import services.UsersService;
 
@@ -18,30 +15,42 @@ public class Posts {
     private int userId;
 
     @Test
-    public void verify_If_user_is_able_to_create_post(){
+    public void verify_If_user_is_able_to_create_post() {
         //Arrange
         userId = UsersService.getUserIdOfUser("Delphine");
 
         //Act & Assert
-        if(userId != 0) {
+        if (userId != 0) {
             PostsService.createPost("Title", "This is my new post", userId)
                     .then().assertThat()
                     .body("title", equalTo("Title"))
                     .body("body", equalTo("This is my new post"))
                     .body("userId", equalTo(userId));
+        } else {
+            Assert.fail("User does not exist, therefore unable to create post.");
         }
-        else{ Assert.fail("User does not exist, therefore unable to create post."); }
     }
 
     @Test
-    public void verify_If_user_is_able_to_update_post(){
+    public void verify_If_user_is_able_to_update_post() {
         //Arrange
         userId = UsersService.getUserIdOfUser("Delphine");
 
         //Act & Assert
-        PostsService.updatePost("Updated Post", "Post updated",userId,1)
+        PostsService.updatePost("Updated Post", "Post updated", userId, 1)
                 .then().assertThat()
                 .body("title", equalTo("Updated Post"))
                 .body("body", equalTo("Post updated"));
+    }
+
+    @Test
+    public void verify_If_user_is_able_to_delete_post() {
+        //Arrange
+        userId = UsersService.getUserIdOfUser("Delphine");
+
+        //Act & Assert
+        PostsService.deletePost(1)
+                .then().assertThat()
+                .body(equalTo("{}"));
     }
 }
